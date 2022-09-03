dcecc3e05338213502c47867225d24be7485425b
yuranl <46729353+yuranl@users.noreply.github.com>
2022-09-03 14:47:03 -0400

    location and time dict

--- src/send.js
+++ src/send.js
@@ -1,6 +1,21 @@
 var config = require('../config');
 var client = require('twilio')(config.accountSid, config.authToken);
 
+const locations = { 1: "1920 Commons", 
+                    2: "English House",
+                    3: "Hill House", 
+                    4: "Houston Market", 
+                    5: "Lauder College House",
+                  }
+const timeSlots = { 1: "5:00 pm",
+                    2: "5:30 pm", 
+                    3: "6:00 pm", 
+                    4: "6:30 pm", 
+                    5: "7:00 pm", 
+                    6: "7:30 pm",
+                    7: "8:00 pm"
+                  }
+
 var send = function(number,msg) {
     console.log("trying to send");
     // Send the notification
@@ -19,4 +34,31 @@ var send = function(number,msg) {
 //   });
 };
 
-exports.send = send;
+const sendAskLocation = function(number) {
+  var msg = "Welcome to the Penn dinner buddy matching system. Please enter your preferred location:\n";
+  for (key in locations) {
+    msg += key
+    msg += ". "
+    msg += locations[key]
+    msg += "\n"
+  };
+  msg += "To enter multiple locations, simply enter multiple numbers."
+  this.send(number, msg);
+}
+
+const sendAskTime = function(number) {
+  var msg = "Great! Now please enter your preferred location:\n";
+  for (key in locations) {
+    msg += key
+    msg += ". "
+    msg += locations[key]
+    msg += "\n"
+  };
+  msg += "To enter multiple locations, simply enter multiple numbers."
+  this.send(number, msg);
+}
+
+
+
+module.exports = [locations, timeSlots, send, sendAskLocation]
+//exports.send = send;
